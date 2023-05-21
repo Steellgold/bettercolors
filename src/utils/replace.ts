8a56ecd8e16d251e18780d3e419418f3ee728b9f
Gaëtan H <steellgold0@gmail.com>
2023-05-21 11:13:33 +0200

    feat(utils): add hexToConsoleColor function to convert hex color to console color

--- src/utils/replace.ts
+++ src/utils/replace.ts
@@ -1,3 +1,15 @@
 export const replaceMultiple = (str: string, replacements: { search: string; replace: string }[]): string => {
   return replacements.reduce((acc, { search, replace }) => acc.replace(new RegExp(search, 'g'), replace), str);
 }
+
+export const hexToConsoleColor = (hex: string): string => {
+  const hexWithoutHash = hex.replace("#", "");
+
+  const red = parseInt(hexWithoutHash.substr(0, 2), 16);
+  const green = parseInt(hexWithoutHash.substr(2, 2), 16);
+  const blue = parseInt(hexWithoutHash.substr(4, 2), 16);
+
+  const consoleColor = `\x1b[38;2;${red};${green};${blue}m`;
+
+  return consoleColor;
+}
